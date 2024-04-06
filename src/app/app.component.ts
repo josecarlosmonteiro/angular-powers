6b560b54dd84ac6033cc6385bcfeb86f39185d4a
Carlos Monteiro <j.de.araujo.monteiro@avanade.com>
2024-04-06 01:08:00 -0300

    first commit + first component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponent } from './components/first/first.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FirstComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'curso-angular';
