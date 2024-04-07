3608feeb56979fcc892d266620c7f0895655cc31
Carlos Monteiro <j.de.araujo.monteiro@avanade.com>
2024-04-07 02:49:15 -0300

    feat: implementar mapa de estilos por tipo de poder.

--- src/app/powers/constants/powerTypes.ts
+++ src/app/powers/constants/powerTypes.ts
@@ -1,7 +1,7 @@
 export const powerTypes = ['fogo', 'gelo', 'natureza'];
 
 export const powerTypeColorsMap: Record<string, string> = {
-  fogo: '#db602c',
-  gelo: '#9dcff5',
-  natureza: '#52b82a',
+  fogo: 'bg-[#db602c] text-white',
+  gelo: 'bg-[#9dcff5] text-white',
+  natureza: 'bg-[#52b82a] text-white',
 };
