c4496e9b8069a64a9acfb5de5fc90329d894f30d
Carlos Monteiro <j.de.araujo.monteiro@avanade.com>
2024-04-07 02:48:50 -0300

    feat: implementar consumo de service de estados.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponent } from './components/first/first.component';
 import { PowersModule } from './powers/powers.module';
+import { PowerStateService } from './powers/services/power-state.service';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,6 @@ import { PowersModule } from './powers/powers.module';
 })
 export class AppComponent {
   title = 'curso-angular';
+
+  constructor(public powerState: PowerStateService) {}
 }
